959d9e073e40b8e5d93989a30720542125b9c95e
rlaehdwn9097 <rlaehdwn9097@naver.com>
2021-01-16 14:33:52 +0900

    Style:Add animations

--- Assets/Scripts/BasicMove.cs
+++ Assets/Scripts/BasicMove.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BasicMove : MonoBehaviour
 {
+
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,11 @@ public class BasicMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+
+        transform.position = transform.position + horizontal * Time.deltaTime * 2;
     }
 }
