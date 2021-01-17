d65564bb80afac49e100a5177b45933103eacf92
Jinho-Choi123 <choijinho817@gmail.com>
2021-01-17 23:13:59 +0900

    fix:fix animation error spinning when two player press diff key

--- Assets/Scripts/BasicMove.cs
+++ Assets/Scripts/BasicMove.cs
@@ -7,80 +7,88 @@ public class BasicMove : MonoBehaviour
 {
 
     public static string winner = "";
-    public Animator animator;
+        public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
          transform.position = new Vector2(30,30);
     }
     
-    public float moveSpeed = 1.0F;
+    public float moveSpeed = 1.0f;
     // Update is called once per frame
     void FixedUpdate()
     {
+        //왼쪽 위
         if(Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W))
         {
-            SetAnimatorInDiagonal();
-            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
-            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);   
+            SetAnimator(-1.0f , 0.0f);
+            SetVector2(Vector2.left , Vector2.up);
         }
+        //왼쪽 아래
         else if(Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S)){
-            SetAnimatorInDiagonal();
-            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
-            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+            SetAnimator(-1.0f , 0.0f);
+            SetVector2(Vector2.left , Vector2.down);
         }
+        //오른쪽 위
         else if(Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W))
         {
-            SetAnimatorInDiagonal();
-            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
-            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+            SetAnimator( 1.0f , 0.0f);
+            SetVector2(Vector2.right , Vector2.up);
         }
+        //오른쪽 아래
         else if(Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.S))
         {
-            SetAnimatorInDiagonal();
-            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
-            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+            SetAnimator( 1.0f , 0.0f);
+            SetVector2(Vector2.right , Vector2.down);
         }
+        //왼쪽
         else if(Input.GetKey(KeyCode.A))
         {   
-            SetAnimatorInStraight();
-            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            SetAnimator(-1.0f , 0.0f);
+            SetVector2(Vector2.left , Vector2.zero);
         }
+        //오른쪽
         else if(Input.GetKey(KeyCode.D))
         {
-            SetAnimatorInStraight();
-            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+            SetAnimator( 1.0f , 0.0f);
+            SetVector2(Vector2.right , Vector2.zero);
         }
+        //위
         else if(Input.GetKey(KeyCode.W))
         {
-            SetAnimatorInStraight();
-            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);    
+            SetAnimator( 0.0f , 1.0f);
+            SetVector2(Vector2.zero , Vector2.up);
         }
+        //아래
         else if(Input.GetKey(KeyCode.S))
         {
-            SetAnimatorInStraight();
-            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+            SetAnimator( 0.0f , -1.0f);
+            SetVector2(Vector2.zero , Vector2.down);
         }
         
+        //input이 없을때.
         if(!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D) &&!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S)){
-            ResetAnimator();
+            SetAnimator( 0.0f , 0.0f);
         }
 
     }
 
-      public void SetAnimatorInDiagonal(){
-        animator.SetFloat("HorizontalBlue", Input.GetAxisRaw("Horizontal"));
-        animator.SetFloat("VerticalBlue", 0);
+    //animation 조정.
+    //보여주고 싶은 animation 방향대로 값을 1,0,-1로 입력해주면 됨.
+    //ex, 오른쪽으로 간다 -> set_x는 1, set_y 는 0
+    //ex, animator를 멈추고 싶다 -> set_x는 0 || set_y는 0
+    public void SetAnimator(float set_x , float set_y){
+        animator.SetFloat("HorizontalBlue", set_x);
+        animator.SetFloat("VerticalBlue", set_y);
     }
 
-    public void SetAnimatorInStraight(){
-        animator.SetFloat("HorizontalBlue", Input.GetAxisRaw("Horizontal"));
-        animator.SetFloat("VerticalBlue", Input.GetAxisRaw("Vertical"));   
-    }
-
-    public void ResetAnimator(){
-        animator.SetFloat("HorizontalBlue", 0);
-        animator.SetFloat("VerticalBlue", 0);
+    //캐릭터의 위치를 옮기는 것.
+    //x는 좌또는 우로 움직이는 것.
+    //y는 상또는 하로 움직이는것.
+    //ex, 오른쪽으로 가면 -> vector_x에는 vector2.right를, vector_y에는 vector2.zero를.
+    public void SetVector2(Vector2 vector_x , Vector2 vector_y){
+        transform.Translate(vector_x * moveSpeed * Time.deltaTime);
+        transform.Translate(vector_y * moveSpeed * Time.deltaTime);
     }
 
     void OnCollisionEnter2D(Collision2D c) { 
