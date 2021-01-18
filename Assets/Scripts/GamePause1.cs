8582502c7abd153fd505583072626ee92335e406
Jinho-Choi123 <choijinho817@gmail.com>
2021-01-18 23:29:26 +0900

    feature:Add pause menu. can resume to game, restart the game, go to menu

---
+++ Assets/Scripts/GamePause1.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamePause1 : MonoBehaviour
+{
+
+    public GameObject maincamera;
+    public GameObject pauseCamera;
+    private bool isPause = false;
+
+    public void SetPause() {
+        if(!isPause) {
+            maincamera.SetActive(false);
+            pauseCamera.SetActive(true);
+            isPause = true;
+            Time.timeScale = 0;
+            //pausePanel.gameObject.SetActive(true);
+        }
+    }
+
+    public void gotoMenu() {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Start");
+    }
+
+    public void playAgain() {
+        Time.timeScale = 1;
+        isPause = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Continue() {
+        if(isPause) {
+            maincamera.SetActive(true);
+            pauseCamera.SetActive(false);
+            isPause = false;
+            Time.timeScale = 1;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseCamera.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Escape)){
+            if(isPause == false) {
+                SetPause();
+            } else {
+                Continue();
+            }
+        }
+
+        
+    }
+}
---
+++ Assets/Scripts/GamePause2.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamePause2 : MonoBehaviour
+{
+
+    public GameObject maincamera1;
+    public GameObject maincamera2;
+    public GameObject maincamera3;
+    public GameObject pauseCamera;
+    private bool isPause = false;
+
+    public void SetPause() {
+        if(!isPause) {
+            pauseCamera.SetActive(true);
+            maincamera1.SetActive(false);
+            maincamera2.SetActive(false);
+            maincamera3.SetActive(false);
+            isPause = true;
+            Time.timeScale = 0;
+            //pausePanel.gameObject.SetActive(true);
+        }
+    }
+
+    public void gotoMenu() {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Start");
+    }
+
+    public void playAgain() {
+        Time.timeScale = 1;
+        isPause = false;
+         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Continue() {
+        if(isPause) {
+            maincamera1.SetActive(true);
+            maincamera2.SetActive(true);
+            maincamera3.SetActive(true);
+            pauseCamera.SetActive(false);
+
+            isPause = false;
+            Time.timeScale = 1;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseCamera.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Escape)){
+            if(isPause == false) {
+                SetPause();
+            } else {
+                Continue();
+            }
+        }
+        
+    }
+}
